382b1b0efc360172c6b9b690ec9da161b1d578cd
Amanda Tjan <amanda.tjan@gmail.com>
2018-03-14 15:48:07 -0600

    Connect server to database and refactor get for projects endpoint to use database

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
 app.use(bodyParser.json());
 app.use(express.static('public'))
 
-app.locals.projects = [
-  {id: 1, name: 'Happy Project'},
-  {id: 2, name: 'So Sleepy Project'}
-];
+// app.locals.projects = [
+//   {id: 1, name: 'Happy Project'},
+//   {id: 2, name: 'So Sleepy Project'}
+// ];
 
 app.locals.palettes = {
   1: [{ projectId: 1, name: 'Warm Colors', colors: ['#3E92CC', '#3E92CC', '#3E92CC', '#3E92CC','#3E92CC'], id: 1}, { projectId: 1, name: 'Color Colors', colors: ['#3E92CC', '#3E92CC', '#3E92CC', '#3E92CC','#3E92CC'], id: 2}],
@@ -23,13 +27,17 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/v1/projects/', (request, response) => {
-  const { projects } = app.locals;
-
-  response.status(200).json({ projects });
+  database('projects').select()
+    .then(projects => {
+      response.status(200).json(projects)
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
 
 app.get('/api/v1/palettes', (request, response) => {
-  
+
 })
 
 app.get('/api/v1/projects/:id/palettes/', (request, response) => {
